6cd3b990ceb2d8fb5b7c4df6556cc7051288332c
Antonio Araujo <antoniowilliam2011@hotmail.com>
2020-01-02 21:30:12 -0300

    Adicionando outros cenarios da feature saque

--- unitarios/spec/ninjaBank/saque_spec.rb
+++ unitarios/spec/ninjaBank/saque_spec.rb
@@ -1,18 +1,21 @@
 class ContaCorrente
-    attr_accessor :saldo
+    attr_accessor :saldo, :mensagem
     
     def initialize(saldo)
        self.saldo = saldo 
     end
     
     def saque(saque)
-        self.saldo -= saque
+        if (saque > self.saldo)
+            self.mensagem = 'Saldo insuficiente para saque'
+        elsif (saque > 700.00)
+            self.mensagem = 'Limite máximo por saque é de R$700'
+        else
+            self.saldo -= saque
+        end
     end    
 
 end
-
-a
-
 
 describe ContaCorrente do
     
@@ -27,5 +30,51 @@ describe ContaCorrente do
                expect(@conta.saldo).to eql 800.00 
             end
         end
+
+        context 'Deu ruim não tenho saldo' do
+            before(:all) do
+                @conta = ContaCorrente.new(0.0)
+                @conta.saque(100.0)
+            end
+
+            it 'vejo a mensagem' do
+                expect(@conta.mensagem).to eql 'Saldo insuficiente para saque'
+            end
+
+            it 'meu saldo final deve ser R$0' do
+                expect(@conta.saldo).to eql 0.0
+            end
+        end
+
+        context 'Tenho saldo mas não suficiente' do
+            before(:all) do
+                @conta = ContaCorrente.new(500.00)
+                @conta.saque(501.00)
+            end
+
+            it 'vejo a mensagem' do
+                expect(@conta.mensagem).to eql 'Saldo insuficiente para saque'
+            end
+
+            it 'meu saldo final deve ser R$500' do
+                expect(@conta.saldo).to eql 500.00
+            end
+        end
+
+        context 'Tentar sacar mais que o permitido por saque' do
+            before(:all) do
+                @conta = ContaCorrente.new(1000.00)
+                @conta.saque(701.00)
+            end
+
+            it 'vejo a mensagem' do
+                expect(@conta.mensagem).to eql 'Limite máximo por saque é de R$700'       
+            end
+
+            it 'meu saldo final deve ser de R$1000' do
+                expect(@conta.saldo).to eql 1000.00
+            end
+        end 
+
     end
 end
